9f5a292c38402ae2e6e050d33e70062435e67888
JeremiePasquis <jeremie@weboneo.fr>
2019-04-15 01:40:49 +0200

    /todolist/add form processing

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Form\TaskType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -22,11 +23,23 @@ class TodoController extends AbstractController
     /**
      * @Route("/todolist/add", name="todo_add")
      */
-    public function todoAdd()
+    public function todoAdd(Request $request)
     {
         // Prépare la création d'un nouveau commentaire
         $task = new Task();
         $form = $this->createForm(TaskType::class, $task);
+
+        // On traite le formulaire s’il a été remplis
+        $form->handleRequest($request);
+        if( $form->isSubmitted() && $form->isValid() ) {
+            // Si le formulaire a été envoyé
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($task);
+            $manager->flush();
+            $this->addFlash("info","Commentaire ajouté");
+            return $this->redirectToRoute('todo');
+        }
+
 
         return $this->render('todo/todo_add.html.twig', [
             'add_form' => $form->createView()
