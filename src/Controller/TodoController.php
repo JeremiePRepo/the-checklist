1307da6264b3e925166f22351e0787250e93f3b1
JeremiePasquis <jeremie@weboneo.fr>
2019-04-15 01:10:47 +0200

    /todolist/add form

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Task;
+use App\Form\TaskType;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TodoController extends AbstractController
 {
@@ -14,6 +16,20 @@ class TodoController extends AbstractController
     {
         return $this->render('todo/index.html.twig', [
             'controller_name' => 'TodoController',
+        ]);
+    }
+
+    /**
+     * @Route("/todolist/add", name="todo_add")
+     */
+    public function todoAdd()
+    {
+        // Prépare la création d'un nouveau commentaire
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        return $this->render('todo/todo_add.html.twig', [
+            'add_form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/TaskType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('deadline')
+            ->add('duration')
+            ->add('repetition')
+            ->add('startDate')
+            ->add('checked')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
