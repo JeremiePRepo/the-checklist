833fd3d7294be161af3b3d67eed0d49b19c62fb3
JeremiePasquis <jeremie@weboneo.fr>
2019-06-14 20:05:42 +0200

    Athentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,7 +32,6 @@ class SecurityController extends AbstractController
     public function logout()
     {
         // Un contrôleur ne peut être vide
-        throw new \Exception('Activer logout dans security.yaml');
+        throw new Exception('Activer logout dans security.yaml');
     }
-
 }
